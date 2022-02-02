6b0c97f3c8141613f02d84c6f5b0814399517d39
cazambrano12 <59524879+cazambrano12@users.noreply.github.com>
2022-02-01 23:01:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'template/header.php';?>
+
+
+<!--Vista de inicio-->
+    <section class="banner">
+        <div class="row">
+            <h2>
+                Hospital Básico de la Zona El Oro<br><br>
+                <div class="change" id="rotate">
+                    <span>Medicina General</span>
+                    <span>Emergencias</span>
+                    <span>Especialidades</span>
+                    <span>Citas Médicas</span>
+                </div>
+                <br>
+            </h2>
+        </div>
+    </section>
+
+<?php include 'template/footer.php';?>
